600025fea50dabd530c5f4f0cfaa628c0a9eba84
InaldoBrandao <inaldopj@gmail.com>
2020-04-18 18:09:26 -0300

    cleaning project setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eu Inaldo Brandão`,
+    description: `Blog pessoal onde abordo tivermos temas, geralmente ligados a tecnologia e desenvolvimento de software.`,
+    author: `@euinaldobrandao`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
